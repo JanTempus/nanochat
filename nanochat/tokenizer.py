f8fa57129de37a733457a0bc39c31664383457ed
Jan Tempus <jan.tempus@gmail.com>
2026-03-24 14:35:21 +0100

    can now point data for different locations

--- nanochat/tokenizer.py
+++ nanochat/tokenizer.py
@@ -483,16 +483,20 @@ class RustBPETokenizer:
 
 def get_tokenizer():
     from nanochat.common import get_base_dir
-    base_dir = get_base_dir()
-    tokenizer_dir = os.path.join(base_dir, "tokenizer")
+    tokenizer_dir = os.environ.get("NANOCHAT_TOKENIZER_DIR")
+    if tokenizer_dir is None:
+        base_dir = get_base_dir()
+        tokenizer_dir = os.path.join(base_dir, "tokenizer")
     return HuggingFaceTokenizer.from_directory(tokenizer_dir)
     # return RustBPETokenizer.from_directory(tokenizer_dir)
 
 def get_token_bytes(device="cpu"):
     import torch
     from nanochat.common import get_base_dir
-    base_dir = get_base_dir()
-    tokenizer_dir = os.path.join(base_dir, "tokenizer")
+    tokenizer_dir = os.environ.get("NANOCHAT_TOKENIZER_DIR")
+    if tokenizer_dir is None:
+        base_dir = get_base_dir()
+        tokenizer_dir = os.path.join(base_dir, "tokenizer")
     token_bytes_path = os.path.join(tokenizer_dir, "token_bytes.pt")
     assert os.path.exists(token_bytes_path), f"Token bytes not found at {token_bytes_path}? It gets written by tok_train.py"
     with open(token_bytes_path, "rb") as f:
