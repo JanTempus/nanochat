02eed92ce4c21e59b7368c6f6bb7705c0a28d6d5
Jan Tempus <jan.tempus@gmail.com>
2026-03-26 11:16:55 +0100

    bugs

--- nanochat/dataset.py
+++ nanochat/dataset.py
@@ -20,7 +20,7 @@ from nanochat.common import get_base_dir
 # The specifics of the current pretraining dataset
 
 # The URL on the internet where the data is hosted and downloaded from on demand
-BASE_URL = "https://huggingface.co/datasets/pietrolesci/finewebedu-20B"
+BASE_URL = "https://huggingface.co/datasets/pietrolesci/finewebedu-20B/resolve/main"
 MAX_SHARD = 21 # the last datashard is shard_06542.parquet
 index_to_filename = lambda index: f"shard_{index:05d}.parquet" # format of the filenames
 base_dir = get_base_dir()
