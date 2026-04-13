5b004dfc087257b56e1b495ea4b676017d508387
Jan Tempus <jan.tempus@gmail.com>
2026-04-13 11:10:25 +0200

    eugh

--- runs/speedrun.sh
+++ runs/speedrun.sh
@@ -76,10 +76,11 @@ DATASET_DOWNLOAD_PID=$!
 echo "Waiting for dataset download to complete..."
 wait $DATASET_DOWNLOAD_PID
 
-# d24 model (slightly undertrained to beat GPT-2 => decrease data:params ratio from compute optimal 10.5 (default) to 8)
-torchrun --standalone --nproc_per_node=8 -m scripts.base_train -- --depth=24 --target-param-data-ratio=8 --device-batch-size=16 --fp8 --run=$WANDB_RUN
-# evaluate the model: CORE metric, BPB on train/val, and draw samples
-torchrun --standalone --nproc_per_node=8 -m scripts.base_eval -- --device-batch-size=16
+
+# run base training and eval the model
+python -m torch.distributed.run --standalone --nproc_per_node=4 -m scripts.base_train -- --depth=12 --target-param-data-ratio=8 --device-batch-size=32 --fp8 --run=$WANDB_RUN
+
+python -m torch.distributed.run --standalone --nproc_per_node=4 -m scripts.chat_eval -- -i sft_
 
 # -----------------------------------------------------------------------------
 # SFT (teach the model conversation special tokens, tool use, multiple choice)
