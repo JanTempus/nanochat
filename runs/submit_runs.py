d6cf8e4296c3a9530ac295e8a2001c027e1268ba
Jan Tempus <jan.tempus@gmail.com>
2026-08-28 15:06:33 +0200

    let us see if the problem was this script or cscs

--- runs/submit_runs.py
+++ runs/submit_runs.py
@@ -3,9 +3,9 @@ from pathlib import Path
 
 
 TOKENIZER_PATHS = [
-    "/capstor/store/cscs/swissai/a0229/jtempus/tokenisation_lp/rounded_tokenizers/cross_over_climbmix400b_s7/vocab_8192/lp_8192_det",
+    "/capstor/store/cscs/swissai/a0229/jtempus/tokenisation_lp/rounded_tokenizers/cross_over_climbmix400b_s7/vocab_8192/lp_8192_bias",
 ]
-TIME_LIMIT = "02:30:00"
+TIME_LIMIT = "03:00:00"
 SEEDS = [43, 44]
 
 repo_dir = Path(__file__).resolve().parents[1]
